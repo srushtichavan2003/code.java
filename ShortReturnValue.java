300f80bb875954ce602f5eab577fab28c18934b2
srushtichavan2003 <158500262+srushtichavan2003@users.noreply.github.com>
2024-02-13 23:43:08 +0530

    Add files via upload

---
+++ ShortReturnValue.java
@@ -0,0 +1,14 @@
+public class ShortReturnValue {
+    public static void main ( string []srv )
+    {
+        short v =10;
+        short v1 =20;
+        int  v2 = v+v1;
+        System.out.println();
+
+
+    }
+
+
+    
+}
