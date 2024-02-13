666d933c32832ef036501407f682d29bc20682a6
srushtichavan2003 <158500262+srushtichavan2003@users.noreply.github.com>
2024-02-13 23:44:16 +0530

    Add files via upload

---
+++ BytePrintNumber.java
@@ -0,0 +1,11 @@
+class BytePrintNumber {
+    public static void main (String[]b)
+    {
+        byte a=10;
+        byte d=20;
+        int c= a+d;
+        System.out.println(c);
+    }
+
+    
+}
