fb293dd0f1a5b37f26137c7047c90a9258779fe3
srushtichavan2003 <158500262+srushtichavan2003@users.noreply.github.com>
2024-02-14 21:28:41 +0530

    Add files via upload

---
+++ CharDouble.java
@@ -0,0 +1,12 @@
+public class CharDouble {
+    public static void main (String[]args)
+    {
+        char a = '@';
+        double b =(double)a;
+        System.out.println(b);
+    }
+    
+}
+
+/*output: compile Time Success
+          Run Time: success 64.0 */
