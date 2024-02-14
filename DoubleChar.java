01cd7d5c3b2d4fef01bb979ead6c9c3180da87cb
srushtichavan2003 <158500262+srushtichavan2003@users.noreply.github.com>
2024-02-14 21:39:33 +0530

    Add files via upload

---
+++ DoubleChar.java
@@ -0,0 +1,13 @@
+public class DoubleChar {
+    public static void main ( String[]args)
+    {
+        double b = 1024.25;
+        char ch = (char)b;
+        System.out.println(ch);
+    }
+    
+}
+
+/* output: Compile Time: Success
+           Run Time: success ?*/
+ 
