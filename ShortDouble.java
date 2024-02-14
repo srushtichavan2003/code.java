5b2d2f21c9fa29ef45643a7bd1efeae7d8d7b3cb
srushtichavan2003 <158500262+srushtichavan2003@users.noreply.github.com>
2024-02-14 21:04:16 +0530

    Add files via upload

---
+++ ShortDouble.java
@@ -0,0 +1,12 @@
+class ShortDouble 
+{
+    public static void main(String[] args)
+    {
+        short age =1235;
+        double age1 =(double) age;
+        System.out.println(age1);
+    }
+}
+
+/*  output: compile time :success
+        Run time: success 1235.0 */
