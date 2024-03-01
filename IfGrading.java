b3c5a890c6841dd40ac4e53a3c57b5fd5dadafc5
Srushti Chavan <158500262+srushtichavan2003@users.noreply.github.com>
2024-03-01 11:12:06 +0530

    Add files via upload

---
+++ IfGrading.java
@@ -0,0 +1,37 @@
+public class IfGrading {
+    public static void main(String[] args) {
+        int marks=95;
+
+        if (marks>=80 && marks<=100) {
+            System.out.println("A+");
+        }
+        if (marks>60 && marks<80) {
+            System.out.println("B+");
+        }
+        if (marks>=40 && marks<60) {
+            System.out.println("C+");
+        }
+        if (marks>=35 && marks<40) {
+            System.out.println("E");
+        }
+        if (marks>=0 && marks<35) {
+            System.out.println("fail");
+        }
+    }
+}       
+        
+            
+/*output:
+ * complie time success
+ * run time success
+ * A+
+ */
+            
+        
+            
+        
+            
+        
+    
+    
+
