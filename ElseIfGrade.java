88de01861566d0af34968b237fc2c018cfc57552
Srushti Chavan <158500262+srushtichavan2003@users.noreply.github.com>
2024-03-01 11:38:45 +0530

    Add files via upload

---
+++ ElseIfGrade.java
@@ -0,0 +1,46 @@
+public class ElseIfGrade {
+    public static void main(String[] args) {
+        
+        int marks=75;
+
+        if (marks>=80 && marks<=100) {
+            System.out.println("A+");
+        }
+        else if (marks>=60 && marks<80) {
+            System.out.println("B+");
+        }
+        else if (marks>=40 && marks<60) {
+            System.out.println("C+");
+        }
+        else if (marks>=35 && marks<40) {
+            System.out.println("E");
+        }
+        else
+    {
+
+        System.out.println("FAIL");
+
+}
+
+    }
+
+}
+
+/*OUTPUT:
+*compile time success
+*run time succes
+*B+
+ */
+
+
+
+        
+            
+        
+            
+        
+        
+    
+
+    
+
