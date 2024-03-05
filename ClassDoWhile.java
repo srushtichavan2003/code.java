e52497c2d896160bda7496a6f21e3c03cc028658
Srushti Chavan <158500262+srushtichavan2003@users.noreply.github.com>
2024-03-05 21:48:24 +0530

    Add files via upload

---
+++ ClassDoWhile.java
@@ -0,0 +1,27 @@
+public class ClassDoWhile {
+    public static void main(String[] args) {
+        int a=1;
+        do{
+            System.out.println(2*a);
+            a++;
+
+        }while(a<=10);
+
+
+    }
+    
+}
+
+/*output:
+2
+4
+6
+8
+10
+12
+14
+16
+18
+20
+ 
+ */
